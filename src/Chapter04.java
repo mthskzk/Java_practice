d328b9591e27de2f2b11c662da1b88b7eda09ab7
hocus <hocus@192.168.1.13>
2023-02-24 20:19:26 +0900

    chapter3-5

---
+++ src/Chapter04.java
@@ -0,0 +1,16 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		String str1;
+		str1 = "kazuki";
+		System.out.println(str1);
+
+		str1 = "tennis";
+		System.out.println(str1);
+
+		int i1;
+		i1 = 3;
+		System.out.println(i1);
+	}
+
+}
---
+++ src/Chapter05.java
@@ -0,0 +1,21 @@
+
+public class Chapter05 {
+	public static void main(String[] args) {
+		long l = 123L;
+		double d = 1.1;
+		boolean b = true;
+		Float f = 0.5f;
+		Integer i = 3;
+		Character c = 'う';
+		System.out.println(l);
+		System.out.println(d);
+		System.out.println(b);
+		System.out.println(f);
+		System.out.println(i);
+		System.out.println(c);
+		
+		int[] array1= {1,2,3,4,5};
+		System.out.println(array1[3]);
+	}
+
+}
