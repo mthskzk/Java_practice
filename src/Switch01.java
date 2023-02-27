3663049432293ed12ce6c85f09e5e26f7852768c
hocus <hocus@hp220702>
2023-02-27 20:35:45 +0900

    finish_07

---
+++ src/Switch01.java
@@ -0,0 +1,31 @@
+
+public class Switch01 {
+	public static void main(String[] args) {
+		int val = 2;
+		switch(val) {
+		case 3:
+			System.out.println("よくできました!");
+			break;
+		case 2:
+			System.out.println("ふつうでした!");
+			break;
+		default:
+			System.out.println("頑張ろう!");
+		}
+	}
+
+}
+
+/*   switch (式（変数や値）){
+ *      case 値:
+ *          ブロック(処理);
+ *          break;
+ *      case 値:
+ *          ブロック(処理);
+ * 		    break;
+ *      default:
+ *          ブロック(処理);
+ *   }
+ *
+ * */
+ 
